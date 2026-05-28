f457cc800e7cd2ea89e024bdf34737aaf90f5747
Ramesh-Seervi <ramesh@imcrinox.com>
2026-05-28 12:35:43 +0530

    added on enew componencts

--- src/app/app-routing/app-routing-module.ts
+++ src/app/app-routing/app-routing-module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { Home } from '../pages/home/home';
 import { Services } from '../pages/services/services';
 import { Contact } from '../pages/contact/contact';
+import { Login } from '../pages/login/login';
 
 const routes: Routes = [
   { path: "", component: Home },
   { path: "services", component: Services },
   { path: "contact", component: Contact },
+  { path: "login", component: Login },
 ]
 
 @NgModule({
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Services } from './pages/services/services';
 import { Contact } from './pages/contact/contact';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
   { path: "", component: Home },
   { path: "services", component: Services },
   { path: "contact", component: Contact },
+  { path: "login", component: Login },
 ];
 
