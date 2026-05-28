988a34d9e8e810c787565f77ffccedf447d1df13
Ramesh-Seervi <ramesh@imcrinox.com>
2026-05-28 13:16:12 +0530

    new chnages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import { Services } from './pages/services/services';
 import { Contact } from './pages/contact/contact';
 import { Login } from './pages/login/login';
 import { Dashboard } from './private/dashboard/dashboard';
+import { Sales } from './private/sales/sales';
+import { Customes } from './private/customes/customes';
+import { Profile } from './private/profile/profile';
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
@@ -12,7 +15,11 @@ export const routes: Routes = [
   { path: "contact", component: Contact },
   { path: "login", component: Login },
   { path: "dashboard", component: Dashboard, canActivate: [authGuard] },
+  { path: "sales", component: Sales, canActivate: [authGuard] },
+  { path: "customes", component: Customes, canActivate: [authGuard] },
+  { path: "profile", component: Profile, canActivate: [authGuard] },
 ];
+
 
 
 
